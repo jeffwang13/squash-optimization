044592c65b1e1f107e56b0d910cb44fee419f546
Jeffrey Wang <jeffrey868@gmail.com>
2019-11-29 13:15:50 -0500

    add second wall bounce and fmincon

--- squash.m
+++ squash.m
@@ -14,31 +14,72 @@
 % Vars
 % robotX - x position of robot (m)
 % robotY - y position of robot (m)
-% opponentX - x position of opponent (m)
-% opponentY - y position of opponent (m)
+% opponentX - x position of opponent (m) (0 - 9.75)
+% opponentY - y position of opponent (m) (0 - 6.4)
 
-function [ result ] = squash(theta_v, theta_h)
+function [ result ] = squash(angles)
     % For now, 5 <= vertical hit angle <= 89, -60 <= horizontal <= 60
     hitZ = 0.482; % m
-    runningSpeed = 3.71; % m/s
-    hitVelocity = 40; % m/s
-    robotX = 5; % m
-    robotY = 5; % m
-    opponentX = 6 ; % m
-    opponentY = 2; % m
+    runningSpeed = 6; % m/s
+    hitVelocity = 40.0; % m/s
+    robotX = 9.25; % m
+    robotY = 3.2; % m
+    opponentX = 8.75; % m
+    opponentY = 1; % m
+    cor = 0.434; % coeficcient of restitution
 
-    hitVZ = hitVelocity * sind(theta_v);
+    hitVZ = hitVelocity * sind(angles(1));
     tHitGround = max(roots([4.905 -hitVZ hitZ]));
-    d2x = hitVelocity * cosd(theta_v) * cosd(theta_h) * tHitGround - robotX;
-    if theta_h <= 0
-        d2y = robotY - hitVelocity * cosd(theta_v) * sind(theta_h) * tHitGround;
-    else
-        d2y = robotY + hitVelocity * cosd(theta_v) * sind(theta_h) * tHitGround;
+    if isreal(tHitGround) == 0
+        result = 100000;
+        return
     end
-    dOpponentFromBall = sqrt((d2x - opponentX).^2 + (d2y - opponentY).^2);
+    reboundVelocity = hitVelocity * cor;
     
+    % Calculate d2y including elasticity and one wall bounce
+    if angles(2) >= 9.32
+        tHitSideWall = (6.4 - robotY) / (hitVelocity * cosd(angles(1)) * sind(angles(2)));
+        d2y = 6.4 + reboundVelocity * cosd(angles(1)) * sind(angles(2)) * (tHitGround - tHitSideWall);
+    elseif angles(2) < -9.32
+        tHitSideWall = (robotY) / (hitVelocity * cosd(angles(1)) * sind(angles(2)));
+        d2y = 0 - (reboundVelocity * cosd(angles(1)) * sind(angles(2)) * (tHitGround - tHitSideWall));
+    else
+        d2y = robotY + hitVelocity * cosd(angles(1)) * sind(angles(2)) * tHitGround;
+    end
+    
+    if d2y < 0
+        d2y = -d2y;
+    elseif d2y > 6.4
+        d2y = 6.4 - (d2y - 6.4);
+    end
+    
+    if d2y < 0
+        d2y = 0;
+    elseif d2y > 6.4
+        d2y = 6.4;
+    end
+
+    % Calculate d2y including elasticity and one wall bounce
+    tHitFrontWall = robotX / (hitVelocity * cosd(angles(1)) * cosd(angles(2)));
+    d2x = reboundVelocity * cosd(angles(1)) * cosd(angles(2)) * (tHitGround - tHitFrontWall);
+    if d2x > 9.75
+        d2x = 9.75;
+    end
+        
+    dOpponentFromBall = sqrt((d2x - opponentX).^2 + (d2y - opponentY).^2);
     
     % Result is the time it takes for the opponent to get to the ball after
     % it lands (s)
-    result = (dOpponentFromBall / runningSpeed) - tHitGround;
+    result = -((dOpponentFromBall / runningSpeed) - tHitGround);
+    playerTime = (dOpponentFromBall / runningSpeed);
+    timeDiff = playerTime - tHitGround;
+    
+    
+    disp(["ballx: " d2x]);
+    disp(["bally: " d2y]);
+    disp(["player time: " playerTime]);
+    disp(["ball time: " tHitGround]);
+    disp(["time diff: " timeDiff]);
+    disp(["thetaV " angles(1)]);
+    disp(["thetaH " angles(2)]);
 end
--- squashMain.m
+++ squashMain.m
@@ -1,5 +1,15 @@
-% squash(0,0,6.4, 9.75, 1, 20)
-x0 = [1,-60];
+% squash([5 18]);
+x0 = [5,0];
+params = [0,0];
+lb = [0, -18.17]; % 0 <= theta_v <= 25.11
+ub = [25.11, 18.17]; % -18.17 <= theta_h <= 18.17
 A = [];
 b = [];
-x = fmincon(@squash, x0, A, b);
+c = [];
+d = [];
+[x] = fmincon(@(params)squash(params), x0, A, b, c, d, lb ,ub);
+
+disp("theta_v: ");
+disp(x(1));
+disp("theta_h: ");
+disp(x(2));
